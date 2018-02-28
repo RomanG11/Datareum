ef2106cd3f896da9b0a9da947c5a7b2edbe2a217
RomanG11 <33402067+RomanG11@users.noreply.github.com>
2018-02-28 14:49:02 +0200

    Update 2_contracts_migration.js

--- DatareumTruffle/migrations/2_contracts_migration.js
+++ DatareumTruffle/migrations/2_contracts_migration.js
@@ -3,7 +3,7 @@ var Crowdsale = artifacts.require("./DatareumCrowdsale.sol");
 
 // var address = web3.eth.accounts[0];
 module.exports = function(deployer) {
-  deployer.deploy(Token,"0x12345","0x54321").then(function(){
+  deployer.deploy(Token,"0xBBBBaAeDaa53EACF57213b95cc023f668eDbA361","0xBBBBaAeDaa53EACF57213b95cc023f668eDbA361").then(function(){
   	return deployer.deploy(Crowdsale,Token.address);
   });
 }
