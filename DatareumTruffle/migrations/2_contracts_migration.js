9fb7c0bdba5d7dd20647708aa60f61b649f6d404
RomanG11 <33402067+RomanG11@users.noreply.github.com>
2018-02-28 14:39:59 +0200

    Add files via upload

--- DatareumTruffle/migrations/2_contracts_migration.js
+++ DatareumTruffle/migrations/2_contracts_migration.js
@@ -3,7 +3,7 @@ var Crowdsale = artifacts.require("./DatareumCrowdsale.sol");
 
 // var address = web3.eth.accounts[0];
 module.exports = function(deployer) {
-  deployer.deploy(Token).then(function(){
+  deployer.deploy(Token,"0x12345","0x54321").then(function(){
   	return deployer.deploy(Crowdsale,Token.address);
   });
 }
